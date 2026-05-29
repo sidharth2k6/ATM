956e9c43b276ade0b09b50f2889d064185a854d2
Sidharth <sidharthacker@gmail.com>
2026-05-30 00:29:23 +0530

    working on GuiAdvance

--- src/ATM/GuiAdvance.java
+++ src/ATM/GuiAdvance.java
@@ -5,22 +5,21 @@ import java.awt.event.*;
 import javax.swing.*;
 public class GuiAdvance extends JFrame implements ActionListener
 {
-     JTextField Text;
+     JTextField text;
     JButton[] numButtons = new JButton[10];
     JButton eq, back,left,right;
-    String s0="",s1="",s2="";
+    String s0="";
     JLabel card;
-
 
      GuiAdvance(){
         setLayout(new FlowLayout());
         setTitle("ATM");
-         setSize(new Dimension(600, 400));
+         setSize(600, 400);
         JPanel panel = new JPanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+
         card = new JLabel("enter card no:");
-        Text = new JTextField(10);
+        text = new JTextField(10);
         eq = new JButton("=");
         back = new JButton("clr");
         left = new JButton("<");
@@ -37,7 +36,7 @@ public class GuiAdvance extends JFrame implements ActionListener
         
         //add
         add(card);
-        add(Text);
+        add(text);
         for(int i=0;i<=9;i++)
         {
             add(numButtons[i]);
@@ -51,15 +50,25 @@ public class GuiAdvance extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
         if (s.charAt(0) >= '0' && s.charAt(0) <= '9') {
-            if (!s0.equals("")) {
-                Text.setText("enter no:");
-            } else {
-                s0 += s;
-            }
-            Text.setText(s0);
+            s0 += s;
+            text.setText(s0);
+//            if (!s0.equals("")) {
+//                text.setText("enter no:");
+//            } else {
+//                s0 += s;
+//            }
+//            text.setText(s0);
         }
+        else if(s.equals("clr")){
+            s0 = "";
+            text.setText(s0);
+        }
+        else if(s.equals("=")){
+            JOptionPane.showMessageDialog(this, "Entered card number: " + s0);
+        }
+
     }
 public static void main(String[] args) {
-        new GuiAdvance();
+        new GuiAdvance().setVisible(true);
     }
 };
