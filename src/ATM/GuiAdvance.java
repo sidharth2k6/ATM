7d3b77abe4d7933e4b2f4bb3224934093f90154e
Sidharth <sidharthacker@gmail.com>
2026-06-01 15:36:15 +0530

    add dailog box system in guiadvance

--- src/ATM/GuiAdvance.java
+++ src/ATM/GuiAdvance.java
@@ -1,5 +1,6 @@
-//package ATM;
+package ATM;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -25,12 +26,12 @@ class GuiAdvance extends JFrame implements ActionListener {
         JPanel deposit = new JPanel();
         deposit.setBackground(Color.WHITE);
         deposit.add(new JLabel("This is Deposit panel"));
-        
+
 
         JPanel withrawal = new JPanel();
         withrawal.setBackground(Color.WHITE);
         withrawal.add(new JLabel("This is Withrawal panel"));
-        
+
 
         cardPanel.add(login, "Red");
         cardPanel.add(deposit, "Green");
@@ -42,9 +43,8 @@ class GuiAdvance extends JFrame implements ActionListener {
         left = new JButton("<");
         right = new JButton(">");
         // number button
-        for(int i=0;i<=9;i++)
-        {
-            numButtons[i]=new JButton(String.valueOf(i));
+        for (int i = 0; i <= 9; i++) {
+            numButtons[i] = new JButton(String.valueOf(i));
             numButtons[i].addActionListener(this);
         }
         nextBtn = new JButton("Next");
@@ -61,9 +61,8 @@ class GuiAdvance extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
         add(cardPanel, BorderLayout.CENTER);
 
-       JPanel btnPanel = new JPanel(new GridLayout(4, 4, 5, 5));
-        for(int i=0;i<=9;i++)
-        {
+        JPanel btnPanel = new JPanel(new GridLayout(4, 4, 5, 5));
+        for (int i = 0; i <= 9; i++) {
             btnPanel.add(numButtons[i]);
         }
         btnPanel.add(back);
@@ -77,19 +76,20 @@ class GuiAdvance extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
-         String s = e.getActionCommand();
+        String s = e.getActionCommand();
 
         if (s.charAt(0) >= '0' && s.charAt(0) <= '9') {
-            s0+=s;
+            s0 += s;
             text.setText(s0);
         } else if (s.equals("C")) {
             s0 = "";
             text.setText("");
         } else if (s.equals("=")) {
-            text.setText("Result:"+s0);
-        } 
-         else if (e.getSource() == nextBtn) {
+            text.setText("Result:" + s0);
+        } else if (e.getSource() == nextBtn) {
             card.next(cardPanel);
         } else if (e.getSource() == prevBtn) {
             card.previous(cardPanel);
@@ -97,10 +97,10 @@ class GuiAdvance extends JFrame implements ActionListener {
         else {
             text.setText("enter number:");
         }
-
     }
+
     public static void main(String[] args)
     {
         new GuiAdvance();
     }    
-};
+}
